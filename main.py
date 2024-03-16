03e122758916474ccbe47d226f58f38f6dca99de
Lukas Treffner <lukas.treffner@gmx.at>
2024-03-16 20:30:24 +0100

    command line arguments skeleton

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import argparse
 
 
 def main(input_file, stage=1):
@@ -7,14 +8,23 @@ def main(input_file, stage=1):
 
 
 if __name__ == "__main__":
-    use_example = False
+    parser = argparse.ArgumentParser(
+                prog='main',
+                description='AdventOfCode main skeleton',
+                )
+    parser.add_argument("-e", "--example", action=argparse.BooleanOptionalAction, default=False, help="Set if you want to run the example")
+    parser.add_argument("-s", "--stage", action='store', default=0, help="Pass the stage you want to run", type=int)
+    args = parser.parse_args()
+
+    use_example = args.example
     file_name = "example" if use_example else "input"
 
-    start_time = time.time()
-    main(file_name, 1)
-    print(f"Stage 1 time: {time.time()-start_time:.10f}")
+    if args.stage == 1 or args.stage == 0:
+        start_time = time.time()
+        main(file_name, 1)
+        print(f"Stage 1 time: {time.time()-start_time:.10f}")
 
-    exit()
-    start_time = time.time()
-    main(file_name, 2)
-    print(f"Stage 2 time: {time.time()-start_time:.10f}")
+    if args.stage == 2 or args.stage == 0:
+        start_time = time.time()
+        main(file_name, 2)
+        print(f"Stage 2 time: {time.time()-start_time:.10f}")
