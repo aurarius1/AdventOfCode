362aa126bed6ae291b9e5772c6ccf32234f8d1ca
Lukas Treffner <lukas.treffner@gmx.at>
2024-12-11 09:01:18 +0100

    2024 11

---
+++ 2024/11/Day11.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Linq.Expressions;
+using _2024.Utils;
+using System.Threading.Tasks;
+
+namespace _2024._11;
+
+public class Day11 : Base
+{
+    public Day11()
+    {
+        Day = "11";
+    }
+
+    private static ValueTuple<ulong, ulong> SplitStone(ulong stone)
+    {
+        ulong digits = (ulong)Math.Log10(stone) + 1;
+        ulong divisor = (ulong)Math.Pow(10, (digits / 2)); 
+        ulong firstStone = stone / divisor;
+        ulong secondStone = stone % divisor;         
+        return (firstStone, secondStone);
+    }
+    
+    private static void SafeAdd(ulong stone, Dictionary<ulong, ulong> stones, ulong toAdd)
+    {
+        stones.TryAdd(stone, 0);
+        stones[stone] += toAdd;
+    }
+
+    private static ulong SolvePuzzle(string input, int stage)
+    {
+        Dictionary<ulong, ulong> stones = input.Split(' ').Select(ulong.Parse)
+            .GroupBy(num => num) // Group by each unique number
+            .ToDictionary(g => g.Key, g => (ulong)g.Count());
+        
+        for (int i = 0; i < ((stage == 1) ? (25) : (75)); i++)
+        {
+            Dictionary<ulong, ulong> stonesAfterBlink = new(stones);
+            foreach (ulong stone in stones.Keys.Where(stone => stones[stone] != 0))
+            {
+                stonesAfterBlink[stone] -= stones[stone];
+                
+                if (stone == 0)
+                {
+                    SafeAdd(stone+1, stonesAfterBlink, stones[stone]);
+                    continue;
+                }
+                
+                // if num digits is even, split
+                if ((((ulong)Math.Log10(stone) + 1) % 2) == 0)
+                {
+                    (ulong first, ulong second) = SplitStone(stone);
+                    SafeAdd(first, stonesAfterBlink, stones[stone]);
+                    SafeAdd(second, stonesAfterBlink, stones[stone]);
+                    continue;
+                }
+
+                SafeAdd(stone * 2024, stonesAfterBlink, stones[stone]);
+            }
+            
+            stones = stonesAfterBlink;
+        }
+
+        return stones.Keys.Aggregate<ulong, ulong>(0, (current, stone) => (current + stones[stone]));
+
+    }
+    
+    
+    public override object PartOne(bool example)
+    {
+        string[] input = ReadInput(example);
+        return SolvePuzzle(input[0], 1);
+    }
+    
+    // this technically runs faster, but the linked list solution looks better and works as well 
+    public override object PartTwo(bool example)
+    {
+        string[] input = ReadInput(example);
+        return SolvePuzzle(input[0], 2);
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  2024 - 20 ⭐
+  2024 - 22 ⭐
 </h1>
 <a href="2024/01/Day01.cs">
   <img src=".aoc_tiles/tiles/2024/01.png" width="161px">
@@ -31,6 +31,9 @@
 </a>
 <a href="2024/10/Day10.cs">
   <img src=".aoc_tiles/tiles/2024/10.png" width="161px">
+</a>
+<a href="2024/11/Day11.cs">
+  <img src=".aoc_tiles/tiles/2024/11.png" width="161px">
 </a>
 <h1 align="center">
   2023 - 50 ⭐
