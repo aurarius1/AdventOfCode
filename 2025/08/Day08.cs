97562cad7601e992beb1c9d65cdd69495128deed
Lukas Treffner <lukas.treffner@student.tugraz.at>
2025-12-08 16:48:22 +0100

    202508 more linq

--- 2025/08/Day08.cs
+++ 2025/08/Day08.cs
@@ -1,10 +1,7 @@
-using System.Diagnostics;
-using System;
+using System.Collections.Immutable;
 
 namespace _2025._08
 {
-   
-
     public sealed class Day08 : Base
     {
         
@@ -14,51 +11,58 @@ namespace _2025._08
             Day = "08";
         }
 
-        private static double GetDistance((int, int, int) a, (int, int, int) b)
+        private static double GetDistance((int X, int Y, int Z) a, (int X, int Y, int Z) b)
         {
             return Math.Sqrt(
-                Math.Pow(a.Item1 - b.Item1, 2) + 
-                Math.Pow(a.Item2 - b.Item2, 2) + 
-                Math.Pow(a.Item3 - b.Item3, 2)
+                Math.Pow(a.X - b.X, 2) + 
+                Math.Pow(a.Y - b.Y, 2) + 
+                Math.Pow(a.Z - b.Z, 2)
             );
         }
 
         private object SolvePuzzle(int part)
         {
-            SortedSet<(double, (int, int, int), (int, int, int))> junctionBoxDistances = [];
+
             List<HashSet<(int, int, int)>> circuits = [];
-            List<(int, int, int)> input = ReadInput().Select(x =>
-            {
-                var split = x.Split(',').Select(y => int.Parse(y)).ToArray();
-                return (split[0], split[1], split[2]);
-            }).ToList();
-
-            foreach (var (junctionBox, index) in input.Select((x, i) => (x, i)))
-            {
-                foreach (var junctionBox2 in input.Skip(index+1))
-                {
-                    double distance = GetDistance(junctionBox, junctionBox2);
-                    junctionBoxDistances.Add((distance, junctionBox, junctionBox2));
-                }
-            }
-
-            var range = part == 1 ? (junctionBoxDistances.Take(Example ? 10 : 1000)) : (junctionBoxDistances);
-            foreach(var currConnection in range)
+            var input = ReadInput()
+                .Select(x =>{
+                    var split = x.Split(',').Select(int.Parse).ToArray();
+                    return (X: split[0], Y: split[1], Z: split[2]);
+                })
+                .ToImmutableArray();
+            
+            var junctionBoxDistances = input
+                    .Enumerate()
+                    .SelectMany(junctionBox => 
+                        input
+                            .Skip(junctionBox.index+1)
+                            .Select(junctionBox2 => 
+                                (
+                                    Distance: GetDistance(junctionBox.item, junctionBox2), 
+                                    JunctionBox: junctionBox.item, 
+                                    JunctionBox2: junctionBox2
+                                )
+                            )
+                        )
+                    .ToImmutableSortedSet()
+                    .Take(part == 1 ? (Example ? 10 : 1000) : int.MaxValue);
+        
+            foreach(var currConnection in junctionBoxDistances)
             {
                 var filtered = circuits
                     .Select((circuit, index) => new { circuit, index })
                     .Where(x => 
-                        x.circuit.Any(y => y == currConnection.Item2 || y == currConnection.Item3 )
+                        x.circuit.Any(y => y == currConnection.JunctionBox || y == currConnection.JunctionBox2 )
                     )
                     .OrderByDescending(x => x.index)
                     .ToList();
                 if (filtered.Count == 0)
                 {
-                    circuits.Add( [currConnection.Item2, currConnection.Item3 ]);
+                    circuits.Add([currConnection.JunctionBox, currConnection.JunctionBox2]);
                     continue;
                 }
-                filtered[0].circuit.Add(currConnection.Item2);
-                filtered[0].circuit.Add(currConnection.Item3);
+                filtered[0].circuit.Add(currConnection.JunctionBox);
+                filtered[0].circuit.Add(currConnection.JunctionBox2);
 
                 for(int i = filtered.Count - 1; i >= 1; i--)
                 {
@@ -71,9 +75,9 @@ namespace _2025._08
                     continue;
                 }
 
-                if (filtered[0].circuit.Count == input.Count)
+                if (filtered[0].circuit.Count == input.Length)
                 {
-                    return currConnection.Item2.Item1 * currConnection.Item3.Item1;
+                    return currConnection.JunctionBox.X * currConnection.JunctionBox2.X;
                 }
             }
 
